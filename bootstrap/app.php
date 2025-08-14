592eea971a994a6ed74ae6dd379ffab2e8c0c362
M Ridwan <mridwan07072002@gmail.com>
2025-08-14 14:08:06 +0700

    Fix storage path for Vercel deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,6 +15,29 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->useStoragePath(env('APP_STORAGE', dirname(__DIR__) . '/storage')) 
-    ->create();
+    });
+
+// Cek kalau jalan di Vercel, storage diarahkan ke /tmp
+if (getenv('VERCEL')) {
+    $tmpStorage = '/tmp/storage';
+    $dirs = [
+        $tmpStorage,
+        $tmpStorage.'/framework',
+        $tmpStorage.'/framework/views',
+        $tmpStorage.'/framework/cache',
+        $tmpStorage.'/framework/sessions',
+        $tmpStorage.'/logs',
+    ];
+
+    foreach ($dirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+    }
+
+    $app->useStoragePath($tmpStorage);
+} else {
+    $app->useStoragePath(env('APP_STORAGE', dirname(__DIR__) . '/storage'));
+}
+
+return $app->create();
